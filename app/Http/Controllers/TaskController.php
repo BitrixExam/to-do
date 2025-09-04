6af8db28bf030236300563eb93ce1961f3b5671c
BitrixExam <d4nyil.titov@yandex.ru>
2025-09-04 19:40:37 +0300

    add fuction getTask

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -36,9 +36,10 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function getTask(string $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        return response()->json($task);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TaskController;
 
 Route::post('/tasks', [TaskController::class, 'createTask']);
 Route::get('/tasks', [TaskController::class, 'getAllTasks']);
+Route::get('/tasks/{id}', [TaskController::class, 'getTask']);
